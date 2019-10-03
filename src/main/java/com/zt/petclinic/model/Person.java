3a04d47f705f33ea7aea3a368c11c2c2bcf2c4de
roberthicks <roberthicksiii@gmail.com>
2019-10-03 10:14:08 -0400

    Added require to name field

--- src/main/java/com/zt/petclinic/model/Person.java
+++ src/main/java/com/zt/petclinic/model/Person.java
@@ -29,6 +29,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class Person extends BaseEntity {
 
     @Column(name = "first_name")
+    @NotEmpty
     private String firstName;
 
     @Column(name = "last_name")
